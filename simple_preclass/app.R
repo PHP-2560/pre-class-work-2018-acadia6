ed771539e4d51dc7f93bbc4c349d54a22323c746
Alex Adia <alex.adia1996@gmail.com>
2018-12-04 16:36:51 -0500

    first draft of simple shiny app

--- simple_preclass/app.R
+++ simple_preclass/app.R
@@ -10,7 +10,7 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage("Z score", "pvalue"
+ui <- fluidPage(
    
    # Application title
    titlePanel("Simple Shiny App"),
@@ -18,16 +18,12 @@ ui <- fluidPage("Z score", "pvalue"
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+        numericInput("scoreInput", "Z Score")
       ),
       
       # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      mainPanel( "P value",
+         return("PVal")
       )
    )
 )
@@ -35,14 +31,7 @@ ui <- fluidPage("Z score", "pvalue"
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+   output$PVal<-2*pnorm(-abs(input$scoreInput))
 }
 
 # Run the application 
